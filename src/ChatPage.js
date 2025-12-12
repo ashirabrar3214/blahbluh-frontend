c41a8e01c71c5fdebbf34b2686644cc287c6853a
ashirabrar3214 <ashirabrar3214@gmail.com>
2025-12-11 18:09:31 -0600

    fixed UI and scrolling

--- src/ChatPage.js
+++ src/ChatPage.js
@@ -365,7 +365,7 @@ const handleSendMessage = async () => {
                   </div>
                 </div>
               );
-            })
+            })}
             <div ref={messagesEndRef} />
           </div>
 
